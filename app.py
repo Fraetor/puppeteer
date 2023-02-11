6244cb3764dde3175c77e048eaeffafb23c2bc96
James Frost <git@frost.cx>
2023-02-11 22:57:41 +0000

    Add logic to message key reciver

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import cv2
 from PIL import Image, ImageTk
 from landmarks import landmarks
 import requests
+import warnings
+
+warnings.simplefilter("ignore", UserWarning)
 
 server_url = "http://127.0.0.1:8888"
 confidence = 0.7
@@ -69,6 +72,7 @@ lmain = tk.Label(frame)
 lmain.place(x=0, y=0)
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
+mp_holistic = mp.solutions.holistic  # Mediapipe Solutions
 pose = mp_pose.Pose(min_tracking_confidence=0.5, min_detection_confidence=0.5)
 
 # Load detection model data
@@ -146,5 +150,131 @@ def detect():
     class_box.configure(text=current_stage)
 
 
-detect()
-window.mainloop()
+cooldown = 0
+
+
+def decide_direction(event_class, prob):
+    global cooldown
+    print(f"Class: {event_class}, Prob: {max(prob)}")
+    cooldown -= 1
+    if max(prob) > 0.99 and cooldown < 1:
+        if event_class == "left_head":
+            signal_keypress("left")
+            cooldown = 10
+        elif event_class == "right_head":
+            signal_keypress("right")
+            cooldown = 10
+
+
+##################
+
+# Initiate holistic model
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
+
+with mp_holistic.Holistic(
+    min_detection_confidence=0.5, min_tracking_confidence=0.5
+) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        # Make Detections
+        results = holistic.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # Export coordinates
+        try:
+            # Extract Pose landmarks
+            pose = results.pose_landmarks.landmark
+            pose_row = list(
+                np.array(
+                    [
+                        [landmark.x, landmark.y, landmark.z, landmark.visibility]
+                        for landmark in pose
+                    ]
+                ).flatten()
+            )
+            # Extract Face landmarks
+            face = results.face_landmarks.landmark
+            face_row = list(
+                np.array(
+                    [
+                        [landmark.x, landmark.y, landmark.z, landmark.visibility]
+                        for landmark in face
+                    ]
+                ).flatten()
+            )
+            # Concate rows
+            row = pose_row + face_row
+            # Make Detections
+            X = pd.DataFrame([row])
+            body_language_class = model.predict(X)[0]
+            body_language_prob = model.predict_proba(X)[0]
+            decide_direction(body_language_class, body_language_prob)
+            # Grab ear coords
+            coords = tuple(
+                np.multiply(
+                    np.array(
+                        (
+                            results.pose_landmarks.landmark[
+                                mp_holistic.PoseLandmark.LEFT_EAR
+                            ].x,
+                            results.pose_landmarks.landmark[
+                                mp_holistic.PoseLandmark.LEFT_EAR
+                            ].y,
+                        )
+                    ),
+                    [640, 480],
+                ).astype(int)
+            )
+            # Get status box
+            cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
+            # Display Class
+            cv2.putText(
+                image,
+                "CLASS",
+                (95, 12),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 0, 0),
+                1,
+                cv2.LINE_AA,
+            )
+            cv2.putText(
+                image,
+                body_language_class.split(" ")[0],
+                (90, 40),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 255, 255),
+                2,
+                cv2.LINE_AA,
+            )
+            # Display Probability
+            cv2.putText(
+                image,
+                "PROB",
+                (15, 12),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 0, 0),
+                1,
+                cv2.LINE_AA,
+            )
+            cv2.putText(
+                image,
+                str(round(body_language_prob[np.argmax(body_language_prob)], 2)),
+                (10, 40),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 255, 255),
+                2,
+                cv2.LINE_AA,
+            )
+        except:
+            pass
+        cv2.imshow("Raw Webcam Feed", image)
+        if cv2.waitKey(10) & 0xFF == ord("q"):
+            break
